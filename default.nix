f252bf141099052a38020368f7f070dd2a56992a
theluckystrike <michaljerzylip90@gmail.com>
2026-08-28 15:44:13 +0800

    Add bln-mcp-server; drop example-package; wire NUR update trigger

--- default.nix
+++ default.nix
@@ -19,7 +19,7 @@
   # flakeModules = { }; # flake-parts modules
   overlays = import ./overlays; # nixpkgs overlays
 
-  example-package = pkgs.callPackage ./pkgs/example-package { };
+  bln-mcp-server = pkgs.callPackage ./pkgs/bln-mcp-server { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
 }
